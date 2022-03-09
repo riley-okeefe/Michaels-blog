a2ed53eaf71dc78e965d7b56ff48d29460ed2a54
Alejandro B <rootbeer1963@hotmail.com>
2022-03-09 18:45:29 -0400

    fixed edit show/hide

--- P1.js
+++ P1.js
@@ -24,8 +24,21 @@ function enter() {
 }
 
 function hide(toHide1, toHide2) {
-    document.getElementById(toHide1).style.display = "none";
-    document.getElementById(toHide2).style.display = "none";
+    
+    var x = document.getElementById(toHide1);
+    var v = document.getElementById(toHide2);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";     
+    }
+
+    if (v.style.display === "none") {
+        v.style.display = "block";
+      } else {
+        v.style.display = "none";     
+      }
+    
 }
 
 function showKeyboard(name) {
@@ -34,6 +47,6 @@ function showKeyboard(name) {
       x.style.display = "block";
     } else {
       x.style.display = "none";
+      
     }
-
 }
