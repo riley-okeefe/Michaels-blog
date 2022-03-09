7b1e5a2104cd653a1523b7463a805a8a7293e03f
Riley Okeefe <rileyjsix@outlook.com>
2022-03-09 15:59:29 -0400

    Comments on javascript.

--- P1.js
+++ P1.js
@@ -28,3 +28,20 @@ function display(toBeShown, toBeHid1, toBeHid2) {
     document.getElementById(toBeHid1).style.display = "none";
     document.getElementById(toBeHid2).style.display = "none";
   }
+
+  // By: Riley OKeefe - creating variables to add event listeners
+//   var switch1 = document.getElementById("firstEditSwitch");
+//   var switch2 = document.getElementById("secondEditSwitch");
+//   var switch3 = document.getElementById("thirdEditSwitch");
+
+//   switch1.addEventListener("change", function(){
+//     display('firstEditSwitch', 'secondEditSwitch', 'thirdEditSwitch');
+//   });
+
+//   switch2.addEventListener("change", function(){
+//     display('secondEditSwitch', 'thirdEditSwitch', 'firstEditSwitch');
+//   });
+
+//   switch3.addEventListener("change", function(){
+//       display('thirdEditSwitch', 'secondEditSwitch', 'firstEditSwitch');
+//   });
